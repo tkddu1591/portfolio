7dc4e7fccca3c87ef56c19bd01fbe38bc01ce3e1
김상엽_집 <tkddu1591@gmail.com>
2023-12-14 18:16:28 +0900

    ci cd check

---
+++ back/src/main/java/com/example/back/config/WebConfig.java
@@ -0,0 +1,24 @@
+package com.example.back.config;
+
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+@EnableWebMvc
+public class WebConfig implements WebMvcConfigurer {
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOrigins("http://localhost:3000",
+                        "http://localhost:8080",
+                        "https://wonderful-genie-12322.netlify.app",
+                        "https://silver-dodol-a6f296.netlify.app/"
+                )
+                .allowedHeaders("*")
+                .allowedMethods("OPTIONS", "GET", "POST", "PUT", "DELETE");
+    }
+}
---
+++ back/src/main/java/com/example/back/controller/MainController.java
@@ -0,0 +1,12 @@
+package com.example.back.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class MainController {
+    @GetMapping("/")
+    public String index() {
+        return "Hello World!";
+    }
+}
