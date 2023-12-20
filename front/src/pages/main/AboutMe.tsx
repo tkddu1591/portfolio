7f74aeaaf556939a58c0e28e1edb9d9be4e2230f
김상엽_집 <tkddu1591@gmail.com>
2023-12-20 22:22:58 +0900

    aboutMe, Skills front 제작 완료

--- front/src/pages/main/AboutMe.tsx
+++ front/src/pages/main/AboutMe.tsx
@@ -1,102 +1,104 @@
-import React, {useState} from "react";
-import {Col, Container, Row} from "reactstrap";
+import React, { useState } from 'react';
+import { Col, Container, Row } from 'reactstrap';
 
 function AboutMe() {
-    const md = 12;
-    const lg = 6;
-    const [itemList, setItemList] = useState([
-        {
-            iTag: 'bi bi-person-fill',
-            title: '이름',
-            content: '김상엽',
-            isATag: false,
-        },
-        {
-            iTag: 'bi bi-calendar2-event-fill',
-            title: '생년월일',
-            content: '1998.10.21',
-            isATag: false,
-        },
-        {
-            iTag: 'bi bi-geo-alt-fill',
-            title: '주소지',
-            content: '부산광역시 남구',
-            isATag: false,
-        },
-        {
-            iTag: 'bi bi-telephone-fill',
-            title: '연락처',
-            content: '010-3013-9397',
-            isATag: true,
-            aTagHref: 'tel:010-3013-9397',
-        },
-        {
-            iTag: 'bi bi-envelope-fill',
-            title: '이메일',
-            content: 'tkddu1591@gmail.com',
-            isATag: true,
-            aTagHref: 'mailto:tkddu1591@gmail.com',
-        },
-        {
-            iTag: 'bi bi-pencil-fill',
-            title: '학력',
-            content: '경성대학교(메카트로닉스공학부)',
-            isATag: false,
-        },
-    ]);
-    return (
-        <Container className={'aboutMe'}>
-            <h1>About Me</h1>
-            <Row>
-                <Col className={'table'} lg={4} md={6}>
-                    <div
-                        className={'my_profile'}
-                        style={{
-                            backgroundImage: `url(${process.env.PUBLIC_URL}/img/main/my_profile.jpg)`,
-                        }}></div>
-                </Col>
-                <Col lg={8} md={6} className={'table'}>
-                    <Row className={'aboutBox'}>
-                        {itemList.map((item, index) => (
-                            <AboutMeItem key={item.title} item={item}></AboutMeItem>
-                        ))}
-                    </Row>
-                </Col>
+  const md = 12;
+  const lg = 6;
+  const [itemList, setItemList] = useState([
+    {
+      iTag: 'bi bi-person-fill',
+      title: '이름',
+      content: '김상엽',
+      isATag: false,
+    },
+    {
+      iTag: 'bi bi-calendar2-event-fill',
+      title: '생년월일',
+      content: '1998.10.21',
+      isATag: false,
+    },
+    {
+      iTag: 'bi bi-geo-alt-fill',
+      title: '주소지',
+      content: '부산광역시 남구',
+      isATag: false,
+    },
+    {
+      iTag: 'bi bi-telephone-fill',
+      title: '연락처',
+      content: '010-3013-9397',
+      isATag: true,
+      aTagHref: 'tel:010-3013-9397',
+    },
+    {
+      iTag: 'bi bi-envelope-fill',
+      title: '이메일',
+      content: 'tkddu1591@gmail.com',
+      isATag: true,
+      aTagHref: 'mailto:tkddu1591@gmail.com',
+    },
+    {
+      iTag: 'bi bi-pencil-fill',
+      title: '학력',
+      content: '경성대학교(메카트로닉스공학부)',
+      isATag: false,
+    },
+  ]);
+  return (
+    <section>
+      <Container className={'aboutMe'}>
+        <h1>About Me</h1>
+        <Row>
+          <Col className={'table'} lg={4} md={6}>
+            <div
+              className={'my_profile'}
+              style={{
+                backgroundImage: `url(${process.env.PUBLIC_URL}/img/main/my_profile.jpg)`,
+              }}></div>
+          </Col>
+          <Col lg={8} md={6} className={'table'}>
+            <Row className={'aboutBox'}>
+              {itemList.map((item, index) => (
+                <AboutMeItem key={item.title} item={item}></AboutMeItem>
+              ))}
             </Row>
-        </Container>
-    );
+          </Col>
+        </Row>
+      </Container>
+    </section>
+  );
 }
 
 function AboutMeItem({ item }) {
-    const md = 12;
-    const lg = 6;
+  const md = 12;
+  const lg = 6;
 
-    const [aColor, setAColor] = useState('dimgrey');
-    return (
-        <Col className={'item'} lg={lg} md={md}>
-            <div className={'icon'}>
-                <i className={item.iTag}></i>
-            </div>
-            <div className={'title'}>
-                <p>{item.title}</p>
-                {!item.isATag ? (
-                    <p>{item.content}</p>
-                ) : (
-                    <a
-                        style={{ color: aColor, transition: '0.5s' }}
-                        onMouseLeave={() => {
-                            setAColor('dimgray');
-                        }}
-                        onMouseEnter={() => {
-                            setAColor('slateblue');
-                        }}
-                        href={item.aTagHref}>
-                        {item.content}
-                    </a>
-                )}
-            </div>
-        </Col>
-    );
+  const [aColor, setAColor] = useState('dimgrey');
+  return (
+    <Col className={'item'} lg={lg} md={md}>
+      <div className={'icon'}>
+        <i className={item.iTag}></i>
+      </div>
+      <div className={'title'}>
+        <p>{item.title}</p>
+        {!item.isATag ? (
+          <p>{item.content}</p>
+        ) : (
+          <a
+            style={{ color: aColor, transition: '0.5s' }}
+            onMouseLeave={() => {
+              setAColor('dimgray');
+            }}
+            onMouseEnter={() => {
+              setAColor('slateblue');
+            }}
+            href={item.aTagHref}>
+            {item.content}
+          </a>
+        )}
+      </div>
+    </Col>
+  );
 }
 
 export default AboutMe;
--- front/src/pages/main/Home.tsx
+++ front/src/pages/main/Home.tsx
@@ -10,48 +10,21 @@ function Home() {
       <Hero />
       <AboutMe></AboutMe>
       <Skills></Skills>
-      <section className="project" >
+      <section className="project">
         <Container>
           <Row className="row-grid align-items-center">
-            <Col className="order-lg-2 ml-lg-auto" md="6">
-              <div className="position-relative pl-md-5">
-                <img alt="..." className="img-center img-fluid" src={require('assets/img/ill/ill-2.svg')} />
-              </div>
-            </Col>
-            <Col className="order-lg-1" lg="6">
-              <div className="d-flex px-3">
-                <div>
-                  <div className="icon icon-lg icon-shape bg-gradient-white shadow rounded-circle text-primary">
-                    <i className="ni ni-building text-primary" />
-                  </div>
-                </div>
-                <div className="pl-4">
-                  <h4 className="display-3 text-white">Modern Interface</h4>
-                  <p className="text-white">
-                    The Arctic Ocean freezes every winter and much of the sea-ice then thaws every summer, and that
-                    process will continue whatever.
-                  </p>
-                </div>
-              </div>
+            <Col className="order-lg-1" lg="12">
+              <h1 style={{ color: 'white' }}>project</h1>
               <Card className="shadow shadow-lg--hover mt-5">
                 <CardBody>
-                  <div className="d-flex px-3">
-                    <div>
-                      <div className="icon icon-shape bg-gradient-success rounded-circle text-white">
-                        <i className="ni ni-satisfied" />
-                      </div>
-                    </div>
-                    <div className="pl-4">
-                      <h5 className="title text-success">Awesome Support</h5>
-                      <p>
-                        The Arctic Ocean freezes every winter and much of the sea-ice then thaws every summer, and that
-                        process will continue whatever.
-                      </p>
-                      <a className="text-success" href="#pablo" onClick={e => e.preventDefault()}>
+                  <Row>
+                    <Col lg={3}>Hello World</Col>
+                    <Col lg={9}>
+                      <a className="text-primary" href="#pablo" onClick={e => e.preventDefault()}>
                         Learn more
                       </a>
-                    </div>
-                  </div>
+                    </Col>
+                  </Row>
                 </CardBody>
               </Card>
               <Card className="shadow shadow-lg--hover mt-5">
@@ -79,17 +52,6 @@ function Home() {
           </Row>
         </Container>
         {/* SVG separator */}
-        <div className="separator separator-bottom separator-skew zindex-100">
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            preserveAspectRatio="none"
-            version="1.1"
-            viewBox="0 0 2560 100"
-            x="0"
-            y="0">
-            <polygon className="fill-white" points="2560 0 2560 100 0 100" />
-          </svg>
-        </div>
       </section>
     </>
   );
