39706aba38b68c6e7e43f11ea485f8243ea0b4c4
김상엽_집 <tkddu1591@gmail.com>
2023-12-21 23:30:39 +0900

    project 영상 및 내용 제작 완료

--- front/src/pages/main/Home.tsx
+++ front/src/pages/main/Home.tsx
@@ -2,7 +2,7 @@ import Hero from '../../views/IndexSections/Hero';
 import React, { useState } from 'react';
 import AboutMe from './AboutMe';
 import Skills from './Skills';
-import { Card, CardBody, Col, Container, Row } from 'reactstrap';
+import { Button, Card, CardBody, Col, Container, Modal, Row } from 'reactstrap';
 
 function Home() {
   return (
@@ -16,6 +16,7 @@ function Home() {
 }
 
 function Project() {
+  const [modal, setModal] = useState('');
   return (
     <section className="project">
       <Container>
@@ -30,31 +31,46 @@ function Project() {
                   </Col>
                   <Col lg={5} className={'content'}>
                     <p>Hello World</p>
-                    <div>
-                      <span>2023.10 - 2021.11</span>
-                    </div>
-                    <div>
-                      <span><strong>Hello World는 개발자 커뮤니티입니다.</strong><br/> 개발자의 필요 강의들의 수강 및 관련 대화를 나눌 수 있는 페이지입니다.</span>
-                    </div>
+                    <span>2023.10 - 2023.11</span>
+                    <div
+                      dangerouslySetInnerHTML={{
+                        __html:
+                          '<span>\n' +
+                            '<strong>Hello World는 개발자 커뮤니티입니다.</strong>\n' +
+                            '<br /> 개발자의 필요 강의들의 수강 및 관련 대화를 나눌 수 있는 페이지입니다.\n' +
+                            '<br />\n' +
+                            '<br /> QueryDSL을 이용한 여러 조건 검색 기능과 typescript를 활용한 프론트 기능 구현을 중심으로\n' +
+                            '작업하였습니다.\n' +
+                            '</span>\n',
+                      }}
+                    />
                     <table>
                       <tbody>
                         <tr>
-                          <td>github</td>
-                          <td>https://github.com/tkddu1591/HelloWorld</td>
+                          <td>Back</td>
+                          <td>Spring boot, JPA, QueryDsl</td>
                         </tr>
                         <tr>
-                          <td>useSkills</td>
-                          <td>dd</td>
+                          <td>Front</td>
+                          <td>React, typescript</td>
                         </tr>
                         <tr>
-                          <td>dd</td>
-                          <td>dd</td>
+                          <td>담당파트</td>
+                          <td>강의, DM</td>
                         </tr>
                       </tbody>
                     </table>
-                    <a className="text-primary" href="#pablo" onClick={e => e.preventDefault()}>
-                      Learn more
-                    </a>
+                    <div className={'icon'}>
+                      <i className="bi bi-youtube youtube" onClick={() => setModal('HelloWorld')}></i>
+                      <ProjectModal title={'HelloWorld'} modal={modal} setModal={setModal}></ProjectModal>
+                      <i
+                        className="bi bi-github github"
+                        onClick={() => {
+                          window.open('https://github.com/tkddu1591/HelloWorld');
+                        }}>
+                        {' '}
+                      </i>
+                    </div>
                   </Col>
                 </Row>
               </CardBody>
@@ -85,6 +101,42 @@ function Project() {
       </Container>
       {/* SVG separator */}
     </section>
+  );
+}
+
+function ProjectModal({ title, modal, setModal }) {
+  return (
+    <Modal className="modal-xl modal-dialog-centered" isOpen={modal === title} toggle={() => setModal('')}>
+      <div className="modal-header">
+        <h4 className="modal-title" id="modal-title-default">
+          Hello World 영상
+        </h4>
+        <button aria-label="Close" className="close" data-dismiss="modal" type="button" onClick={() => setModal(false)}>
+          <span aria-hidden={true}>×</span>
+        </button>
+      </div>
+      <div className="modal-body">
+        <h5>프로젝트 시연영상</h5>
+        <div className={'youtube-div'}>
+          <iframe
+            className={'youtube-player'}
+            width="640"
+            height="360"
+            src="https://www.youtube.com/embed/Q8xfKjQxjg8?si=rbMl-G4fS4jtIM-n"
+            allowFullScreen={true}></iframe>
+        </div>
+
+        <h5>프로젝트 발표영상</h5>
+        <div className={'youtube-div'}>
+          <iframe
+            className={'youtube-player'}
+            width="640"
+            height="360"
+            src="https://www.youtube.com/embed/Q8xfKjQxjg8?si=rbMl-G4fS4jtIM-n"
+            allowFullScreen={true}></iframe>
+        </div>
+      </div>
+    </Modal>
   );
 }
 
